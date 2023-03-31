a283eaf9d8117b93c615734e8af01b75a7e16b88
neha2793 <nehaaguptaa93@gmail.com>
2023-03-31 14:32:57 +0530

    updated project on 27th march

--- pages/map-slime-seat.js
+++ pages/map-slime-seat.js
@@ -1,10 +1,308 @@
+/* pages/create-nft.js */
 import * as React from "react";
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import toast from "../components/Toast";
+import { useEffect, useState } from 'react'
+import Mainheader from './components/layout/mainheader';
+import Mainfooter from './components/layout/mainfooter';
+import queryString from 'query-string';
 
-export default function MapSlimeSeat() {
+import * as $ from 'jquery';
+
+import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
+
+
+
+
+
+
+export default function SlimeSeatMaping() {
+  
+  
+
+  const { SearchBar } = Search;
+  const [videoList, setSCVideoList]=useState([])
+
+  const [show, setShow] = useState(false);
+  const [base_url, BaseUrl]=useState('')
+  const [html, Sethtml] = useState('');
+  const [search, setSearch] = useState('');
+  const [filteredSlimeSeat, setFilteredSlimeSeat] = useState([]);
+
+  const router = useRouter()
+  useEffect(() => {
+    SCVideoList()
+    var table =  document.querySelector('#data-table-ID');
+    table.classList.remove("table-bordered");
+  }, [])
+
+
+  // Get SC Video List .....
+  async function SCVideoList() {
+   
+    const user = JSON.parse(localStorage.getItem('user'));
+    let params = queryString.parse(location.search)
+    if(user){
+      var fd = new FormData();
+      fd.append('slime_seat', params.id);
+      axios.post(process.env.NEXT_PUBLIC_BASE_URL+'/api/get-manage-sc-list',fd,{
+        headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '+user.access_token
+        },
+      }).then( async (response)  => {
+        const data = response.data;
+        BaseUrl(response.data.base_url)
+        setSCVideoList(Object.values(data.manage_sc))
+        setFilteredSlimeSeat(Object.values(data.manage_sc))
+        setTimeout(() => {
+          Object.values(data.manage_sc).map((item , index)=>{
+            if(Boolean(item.SCplacement_id)){ 
+              if(document.getElementById(index)){
+                document.getElementById(index).checked = true;
+              }
+            }
+          })
+        }, 200);
+       
+      }).catch(function (error) {
+        // handle error
+        console.log(error)
+        if(error.response.status == 400){
+          notify("error", error.response.data.error)
+        }else if(error.response.status == 401){
+          localStorage.removeItem("user")
+          router.push('/login')
+        }else{
+          notify("error", 'Something went wrong please try again!')
+        }
+      })
+    }else{
+      router.push('/login')
+    }
+  }
+
+  const notify = React.useCallback((type, message) => {
+    toast({ type, message });
+  }, []);
+
+  // Sc Video Uploaded Here......
+  async function Submit(){
+    const user = JSON.parse(localStorage.getItem('user'));
+    let params = queryString.parse(location.search)
+    var arr= []
+    $('.checkboxs').each(function(index){
+      console.log(index)
+      if($('#'+index).is(':checked')){
+        var checkedBoxes = $('#'+index).val();
+        arr.push(checkedBoxes)
+      }
+    })
+    let fd = new FormData();
+    fd.append('SC_ID', arr)
+    fd.append('Item_ID', params.id)  
+    fd.append('Item_type', 'Slime Seat')  
+    axios({
+      method: 'post',
+      url: process.env.NEXT_PUBLIC_BASE_URL+'/api/map-slime-seat',
+      data: fd,
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        'Authorization': 'Bearer '+user.access_token
+      },
+    }).then((response)  => {
+     if(response.data.status == 204){
+      notify("success", 'Unmap shipping container successfully')
+     }else{
+      notify("success", 'Map shipping container successfully')
+     }
+     
+    }).catch(function (error) {
+      // handle error
+      console.log(error)
+      if(error.response.status == 400){
+        notify("error", error.response.data.error)
+      }else if(error.response.status == 401){
+        localStorage.removeItem("user")
+        router.push('/login')
+      }else{
+        notify("error", 'Something went wrong please try again!')
+      }  
+    })
+  }
+
+  const inputsHandler = (index) =>{    
+
+    var temp = [];
+    filteredSlimeSeat.forEach((value, i) => {
+      if(i == index){
+        value.SCplacement_id = '';
+      }
+      temp.push(value);
+    });
+    setFilteredSlimeSeat(temp)
+
+  }
+
+
+
+
+  // Popup close ....
+  function close(){
+    setShow(false);
+    Sethtml('')
+  }
+
+
+  // Datalist
+  const columns = [
+
+    {
+      text: "#",
+      dataField: 'Sc_ID',
+      headerStyle: { minWidth: '50px' },
+      style: { width: '50px' },
+      formatter: (cell, row, index) => <><input type="checkbox" defaultChecked={Boolean(row.SCplacement_id) } onChange={() => inputsHandler(index)} className="checkboxs" name="checkbox"   value={row.Sc_ID}  id={index}  /><label htmlFor={index}></label></>
+    },
+    {
+      text: "Image",
+      dataField: 'Featured_Image',
+      headerStyle: { minWidth: '50px' },
+      style: { width: '50px' },
+      formatter: (cell, row) => <><div className="video_button"><img src={process.env.NEXT_PUBLIC_BASE_URL+'/public/'+row.Featured_Image} /></div></>,
+    },
+    {
+      text: "Name",
+      dataField: 'Name',
+      sort: true,
+      headerStyle: { minWidth: '150px' },
+      style: { width: '150px' },
+    },
+    {
+      text: "Description",
+      dataField: 'Description',
+      sort: true,
+      style:{width : '504px'},
+      headerStyle:{minWidth : '504px'}
+    },
+   
+    
+  ] 
+  const customTotal = (from, to, size) => (
+    <span className="react-bootstrap-table-pagination-total">
+      Showing { from } to { to } of { size } Results
+    </span>
+  );
+  const options = {
+    paginationSize: 4,
+    pageStartIndex: 1,
+    // alwaysShowAllBtns: true, // Always show next and previous button
+    // withFirstAndLast: false, // Hide the going to First and Last page button
+    // hideSizePerPage: true, // Hide the sizePerPage dropdown always
+    // hidePageListOnlyOnePage: true, // Hide the pagination list when only one page
+    firstPageText: 'First',
+    prePageText: 'Back',
+    nextPageText: 'Next',
+    lastPageText: 'Last',
+    nextPageTitle: 'First page',
+    prePageTitle: 'Pre page',
+    firstPageTitle: 'Next page',
+    lastPageTitle: 'Last page',
+    showTotal: true,
+    paginationTotalRenderer: customTotal,
+    disablePageTitle: true,
+    // A numeric array is also available. the purpose of above example is custom the text
+  };
+
+
+
   return (
     <div>
-      <h1>Map Slime seat</h1>
-    </div>
+    <Mainheader />
+    <section className="login-page" style= {{ background: "url(" + "assets/images/loginbg.jpg" + ")" }}>
+      <section className="innerbg">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h3>Map Shipping Container</h3>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      {/* For Videos List */}
+      <div className="container" >
+        <div className="row video_heading">
+          <div className="col-md-6">
+            <h2>Shipping Container List</h2>
+          </div>
+          <div className="col-md-6 text-right">
+            
+            {/* <button type="button" className="btn btn-info add_sc" onClick={divHandle}>Add Shipping Container</button> */}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="table-responsive datatables-design">
+           
+              <ToolkitProvider
+                keyField="id"
+                data={ filteredSlimeSeat }
+                columns={ columns }
+                search
+              >
+                {
+                  props => (
+                    <div> 
+                      <SearchBar { ...props.searchProps } />
+                      <BootstrapTable 
+                        keyField='id'
+                        id = "data-table-ID"
+                        classes = "dt-responsive dataTable no-footer"
+                        columns={columns}  
+                        data={filteredSlimeSeat} 
+                        pagination={ paginationFactory(options) }
+                        noDataIndication="No Records Found"
+                        { ...props.baseProps }
+                      />
+                    </div>
+                  )
+                }
+              </ToolkitProvider>
+              
+              
+            </div>
+            <div className="tablebtn text-right">
+              <button type="button" className="btn btn-info" onClick={Submit} >Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Video Popup */}
+      <div id="modal-row" className="modal" style={{ display: show ? "block" : "none" }}>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">Shipping Container</h4>
+              <button type="button" className="close" data-dismiss="modal" onClick={close}>&times;</button>
+            </div>
+
+            <div className="modal-body">
+              {html}
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </section>
+    <Mainfooter />
+  </div>
    
   )
 }
--- pages/transaction-history.js
+++ pages/transaction-history.js
@@ -1,10 +1,231 @@
+/* pages/create-nft.js */
 import * as React from "react";
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import toast from "../components/Toast";
+import { useEffect, useState , useRef} from 'react'
 
-export default function Transaction() {
+import Mainheader from './components/layout/mainheader';
+import Mainfooter from './components/layout/mainfooter';
+
+import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
+
+
+
+
+
+
+
+export default function TransactionHistory() {
+  
+  const { SearchBar } = Search;
+  const router = useRouter()
+  const dataFetchedRef = useRef(false);
+  const [FilteredTransactionHostory, setFilteredTransactionHostory] = useState([]);
+  const [Flag, setFlag] = useState(false);
+
+ 
+  useEffect(() => {
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
+
+    GetTransactionHistory()
+    var table =  document.querySelector('#data-table-ID');
+    table.classList.remove("table-bordered");
+
+  }, [FilteredTransactionHostory])
+
+  const notify = React.useCallback((type, message) => {
+    toast({ type, message });
+  }, []);
+
+  async function setData(data, USDPrice){
+    Object.values(data).forEach(async (value, index) => {
+      value.USDPrice = value.T_Amount * USDPrice;
+    });
+    return data;
+  }
+
+  // Get SC Video List .....
+  async function GetTransactionHistory() {
+
+  
+    const user = JSON.parse(localStorage.getItem('user'));
+    if(user){
+      try{
+        var response = await axios.get(process.env.NEXT_PUBLIC_BASE_URL+'/api/get-transaction-history', {
+          headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer '+user.access_token
+          },
+        }) 
+        const data = await response.data; 
+
+        const perUSDPrice = await axios.get('https://api.coinconvert.net/convert/matic/usd?amount='+1) 
+        var dataWithUSD = await setData(data.data, perUSDPrice.data.USD)
+        console.log(dataWithUSD)
+        setFilteredTransactionHostory(dataWithUSD);
+
+      }catch(error) {
+        // handle error
+        console.log(error)
+        if(error.response.status == 400){
+          notify("error", error.response.data.error)
+        }else if(error.response.status == 401){
+          localStorage.removeItem("user")
+          router.push('/login')
+        }else{
+          notify("error", 'Something went wrong please try again!')
+        }
+      }
+    }else{
+      router.push('/login')
+    }
+  }
+
+  // Datalist
+  const columns = [
+    {
+      text: "#",
+      dataField: 'id',
+      formatter: (cell, row, index) => index+1,
+      
+    },
+    {
+      text:  "Transaction ID",
+      dataField: 'Transaction_Token',
+      sort: true,
+      headerStyle: { minWidth: '120px' },
+      style: { width: '120px' },
+      formatter: (cell, row) => row.Transaction_Token
+    },
+    {
+      text: "Name",
+      dataField: 'name',
+      sort: true,
+      headerStyle: { minWidth: '150px' },
+      style: { width: '150px' },
+      formatter: (cell, row) => row.name
+    },
+    {
+      text: "Matic Price",
+      dataField: 'T_Amount',
+      sort: true,
+      headerStyle: { minWidth: '90px' },
+      formatter: (cell, row) => row.T_Amount+'Matic'
+    },
+    {
+      text: "USD Price",
+      dataField: 'USDPrice',
+      sort: true,
+      headerStyle: { minWidth: '90px' },
+      formatter: (cell, row) =>row.USDPrice? '$ '+row.USDPrice.toFixed(7): 'Not Found'
+    },
+    {
+      text: "Status",
+      dataField: 'Status',
+      sort: true,
+      formatter: (cell, row) => row.Status
+    },
+    
+    
+  ] 
+  const customTotal = (from, to, size) => (
+    <span className="react-bootstrap-table-pagination-total">
+      Showing { from } to { to } of { size } Results
+    </span>
+  );
+  const options = {
+    paginationSize: 4,
+    pageStartIndex: 1,
+    // alwaysShowAllBtns: true, // Always show next and previous button
+    // withFirstAndLast: false, // Hide the going to First and Last page button
+    // hideSizePerPage: true, // Hide the sizePerPage dropdown always
+    // hidePageListOnlyOnePage: true, // Hide the pagination list when only one page
+    firstPageText: 'First',
+    prePageText: 'Back',
+    nextPageText: 'Next',
+    lastPageText: 'Last',
+    nextPageTitle: 'First page',
+    prePageTitle: 'Pre page',
+    firstPageTitle: 'Next page',
+    lastPageTitle: 'Last page',
+    showTotal: true,
+    paginationTotalRenderer: customTotal,
+    disablePageTitle: true,
+    // A numeric array is also available. the purpose of above example is custom the text
+  };
+
+  
+  
+  
+
+
   return (
     <div>
-      <h1>transaction</h1>
-    </div>
+    <Mainheader />  
+    <section className="login-page" style= {{ background: "url(" + "assets/images/loginbg.jpg" + ")" }}>
+      <section className="innerbg">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h3>Transaction History</h3>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      {/* For Slime seat  List */}
+      <div className="container" >
+        <div className="row video_heading">
+          <div className="col-md-6">
+            <h2>Transaction History list</h2>
+          </div>
+          <div className="col-md-6 text-right">
+            
+            {/* <button type="button" className="btn btn-info add_sc" >Add Slime Seat Management</button> */}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="table-responsive datatables-design">  
+              <ToolkitProvider
+                  keyField="id"
+                  data={ FilteredTransactionHostory }
+                  columns={ columns }
+                  search
+                >
+                  {
+                    props => (
+                      <div> 
+                        <SearchBar { ...props.searchProps }
+                        />
+                        <BootstrapTable 
+                          keyField='id'
+                          id = "data-table-ID"
+                          classes = "dt-responsive dataTable no-footer"
+                          columns={columns} 
+                          data={FilteredTransactionHostory} 
+                          pagination={ paginationFactory(options) }
+                          noDataIndication={ "No Records Found"}
+                          { ...props.baseProps }
+                        />
+                      </div>
+                    )
+                  }
+              </ToolkitProvider>
+             
+               
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <Mainfooter />
+  </div>
    
   )
 }
